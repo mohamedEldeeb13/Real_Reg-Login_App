c836f2be0b30e26f8e0049ec285b9c23f3af110c
mohamedEldeeb13 <mooel744@gmail.com>
2024-03-06 11:03:25 +0200

    Initial Commit

---
+++ Real_Reg&Login_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Real_Reg&Login_App
+//
+//  Created by Mohamed Abd Elhakam on 06/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Real_Reg&Login_App/Real_Reg_Login_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_Reg_Login_AppApp.swift
+//  Real_Reg&Login_App
+//
+//  Created by Mohamed Abd Elhakam on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Real_Reg_Login_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
